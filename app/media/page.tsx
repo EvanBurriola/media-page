cbfc896aa8b0d7e15b2877844a08c2fee62304d1
Evan Burriola <evanburriola12@tamu.edu>
2024-01-30 13:14:59 -0600

    Tree horizontal, main page addition

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 import Tree from "@/components/tree";
 import React, { useState, useEffect } from "react"
 
-const treeData = [
+const musicTree = [
   {
     key: "0",
     label: "Music",
     children: [
       {
         key: "0-0",
-        label: "Streaming",
+        label: "Listening",
         children: [
           {
             key: "0-1-1",
@@ -68,7 +68,7 @@ const treeData = [
       },
       {
         key: "0-1",
-        label: "Performing",
+        label: "Creating",
         children: [
           {
             key: "0-1-1",
@@ -107,44 +107,123 @@ const treeData = [
       
     ],
   },
+];
+
+const videoTree = [
   {
     key: "1",
     label: "Video",
     children: [
       {
         key: "1-0",
-        label: "document1.doc",
+        label: "YouTube",
+        children: [
+          {
+            key: "1-0-0",
+            label: "Music"
+          },
+          {
+            key: "1-0-1",
+            label: "Programming/Math"
+          },
+          {
+            key: "1-0-2",
+            label: "Gaming"
+          }
+        ]
       },
       {
-        key: "0-0",
-        label: "documennt-2.doc",
+        key: "1-1",
+        label: "Instagram",
+        children: [
+          {
+            key: "1-1-0",
+            label: "Music"
+          },
+          {
+            key: "1-1-1",
+            label: "Friends/Family"
+          },
+          {
+            key: "1-1-2",
+            label: "Gaming"
+          }
+        ]
       },
     ],
   },
+];
+
+const textTree = [
   {
     key: "2",
     label: "Text",
     children: [
       {
-        key: "1-0",
-        label: "document1.doc",
-      }
+        key: "2-0",
+        label: "Books",
+        children: [
+          {
+            key: "2-0-0",
+            label: "Fantasy",
+            children: [
+              {
+                key: "2-0-0-0",
+                label: "Dragonlance: Chronicles"
+              },
+              {
+                key: "2-0-0-1",
+                label: "Legend of Drizzt"
+              },
+            ]
+          }
+        ]
+      },
+      {
+        key: "2-1",
+        label: "Internet",
+        children: [
+          {
+            key: "2-1-0",
+            label: "Messages",
+          },
+          {
+            key: "2-1-1",
+            label: "Text"
+          },
+          {
+            key: "2-1-2",
+            label: "Email"
+          },
+          {
+            key: "2-1-3",
+            label: "Social Media"
+          },
+          {
+            key: "2-1-4",
+            label: "Forums"
+          }
+        ]
+      },
     ],
   },
+];
+
+const imageTree = [
   {
     key: "3",
-    label: "Picture",
+    label: "Image",
     children: [
       {
-        key: "1-0",
-        label: "document1.doc",
+        key: "3-0",
+        label: "Instagram",
       },
       {
-        key: "0-0",
-        label: "documennt-2.doc",
+        key: "3-1",
+        label: "Snapchat",
       },
     ],
-  }
+  }  
 ];
 
 export default function MediaTree() {
@@ -168,18 +247,23 @@ export default function MediaTree() {
   const treeComponent = (expand : boolean) => {
       return (
         <>
-          <div className="w-screen absolute -z-10 blur-3xl">
-            <img src="innerChamber.jpg" alt="Inner Chamber" className="object-fill min-h-screen absolute saturate-50"></img>
+          <div className="w-full min-h-screen absolute -z-10 blur-3xl">
+            <img src="innerChamber.jpg" alt="Inner Chamber" className="object-fill absolute saturate-50 select-none"></img>
           </div>
           <main className="flex min-h-screen flex-col items-center p-4">
-            <div className="mb-3 flex flex-row">
+            <div className="mb-3 flex flex-row pr-20">
               <Link href='/' className="mt-4 mr-4 w-20 h-6 text-center text-white hover:bg-blue-800 bg-blue-600 transition-colors duration-300 rounded-xl drop-shadow-[0_8.2px_1.2px_rgba(0,0,0,0.8)] select-none">Home</Link>
               <h1 className="select-none drop-shadow-[0_8.2px_1.2px_rgba(0,0,0,0.8)] text-white text-6xl">Media Breakdown</h1>
               <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer select-none mt-6 text-white">
               <path strokeLinecap="round" strokeLinejoin="round" d={expand == true ? "m4.5 15.75 7.5-7.5 7.5 7.5" : "m19.5 8.25-7.5 7.5-7.5-7.5" } />
               </svg>
             </div>
-              <Tree treeData={treeData} expand={expand} />
+            <div className="flex flex-row w-fill gap-x-12">
+              <Tree treeData={musicTree} expand={expand} />
+              <Tree treeData={videoTree} expand={expand} />
+              <Tree treeData={textTree} expand={expand} />
+              <Tree treeData={imageTree} expand={expand} />
+            </div>
           </main>
         </>
       );  
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </div>
 
       <div className="flex flex-row h-screen justify-between">
-        <div className="flex flex-col h-fit w-2/4 pt-24">
+        <div className="flex flex-col h-fit w-2/4 pt-16">
 
           <NameBanner></NameBanner>
 
@@ -21,7 +21,12 @@ export default function Home() {
             and seemed interesting. I am enjoying discussing the social implications of certain media technologies and am excited to see what we continue to study.</p>
           </div>
 
-          <Link href='/media' className="flex justify-center mx-12 mt-12 text-white hover:bg-blue-800 bg-blue-600 transition-colors duration-300 rounded-3xl drop-shadow-[0_8.2px_1.2px_rgba(0,0,0,0.8)] select-none">Media Use Tree</Link>
+          <h2 className="flex justify-center mx-12 mt-6 text-white text-4xl font-bold drop-shadow-[0_8.2px_1.2px_rgba(0,0,0,0.8)] select-none">Connectedness and Music</h2>
+          <div className="w-fit h-fit mx-12 mt-2 bg-white bg-opacity-40">
+            <p className="px-2 text-black text-2xl select-none">I think there should be better tools for music sharing and discovery as a social media.</p>
+          </div>
+
+          <Link href='/media' className="flex justify-center mx-12 mt-4 text-white hover:bg-blue-800 bg-blue-600 transition-colors duration-300 rounded-3xl drop-shadow-[0_8.2px_1.2px_rgba(0,0,0,0.8)] select-none">Media Use Tree</Link>
           
         </div>
 
